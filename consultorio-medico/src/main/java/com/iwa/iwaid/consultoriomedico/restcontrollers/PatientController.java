5db4703aee32ac0d8ebacde92ae19faa77c7b4f4
Irving Fuentes <cronox.fc3@gmail.com>
2023-07-24 22:54:44 -0600

    Implementation of the PatientForm and PatientDTO

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/restcontrollers/PatientController.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/restcontrollers/PatientController.java
@@ -1,6 +1,7 @@
 package com.iwa.iwaid.consultoriomedico.restcontrollers;
 
-import com.iwa.iwaid.consultoriomedico.entity.Patient;
+import com.iwa.iwaid.consultoriomedico.entity.PatientDTO;
+import com.iwa.iwaid.consultoriomedico.entity.PatientForm;
 import com.iwa.iwaid.consultoriomedico.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,17 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-/** The PatientController class, marked with the '@RestController' annotation, is a REST controller
- *  where every method returns a domain object instead of a view.
- * It's mapped to handle web requests at the '/iwaid/patients/' path, as specified by the @RequestMapping annotation.
- */
+
 @RestController
 @RequestMapping(path = "/iwaid/patients/")
 public class PatientController {
 
-    /** The PatientService is injected into this controller using the '@Autowired' annotation.
-     * The constructor is used to assign the PatientService to a final field in the controller.
-     */
     @Autowired
     private final PatientService patientService;
 
@@ -27,65 +22,37 @@ public class PatientController {
         this.patientService = patientService;
     }
 
-    /** The getbyId() method, mapped to a GET request at the '/iwaid/patients/{id} path, retrieves a specific
-     *  patient by their ID.
-     * It calls the getPatient method from PatientService, passing the ID from the path variable.
-     * If a patient with the given ID exists, it returns the patient and a status of OK.
-     * If not, it returns a 'Patient not found' message and a status of NOT FOUND.
-     */
     @GetMapping("/{id}")
-    public ResponseEntity<?> getbyId(@PathVariable int id){
-        Optional<Patient> patient = patientService.getPatient(id);
-        if(patient.isPresent()){
+    public ResponseEntity<PatientDTO> getbyId(@PathVariable int id) {
+        Optional<PatientDTO> patient = Optional.ofNullable(patientService.getPatient(id));
+        if (patient.isPresent()) {
             return new ResponseEntity<>(patient.get(), HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("Patient not found", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    /** The save() method, mapped to a POST request at the '/iwaid/patients/' path,
-     * saves a new patient to the database.
-     * It calls the savePatient method from PatientService, passing the Patient object from the request body.
-     * It returns the ID of the saved patient and a status of CREATED.
-     */
     @PostMapping("/")
-    public ResponseEntity<?> save(@RequestBody Patient patient){
-        int id = patientService.savePatient(patient);
-        return new ResponseEntity<>(id, HttpStatus.CREATED);
+    public ResponseEntity<PatientDTO> savePatient(@RequestBody final PatientForm patientForm) {
+        PatientDTO patientDTO = patientService.savePatient(patientForm);
+        return new ResponseEntity<>(patientDTO, HttpStatus.CREATED);
     }
 
-    /**The update() method, mapped to a PUT request at the '/iwaid/patients/{id}' path, }
-     * updates an existing patient in the database.
-     * It achieves this by calling the updatePatient method from the PatientService class,
-     * passing the Patient object from the request body and the ID from the path variable.
-     * The updatePatient() method returns an Optional containing the updated patient if the update is successful,
-     * or an empty Optional if the patient does not exist.
-     * If the update is successful, this method returns a status of CREATED.
-     * If the patient does not exist, it returns a status of NOT FOUND.
-     */
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody Patient patient, @PathVariable int id){
-        Optional<Patient> patient1 = patientService.updatePatient(patient, id);
-        if(patient1.isPresent()){
+    public ResponseEntity<?> update(@RequestBody PatientForm patientForm, @PathVariable int id) {
+        Optional<PatientDTO> patient1 = Optional.ofNullable(patientService.updatePatient(patientForm, id));
+        if (patient1.isPresent()) {
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    /** The delete() method, mapped to a DELETE request at the '/iwaid/patients/{id}' path,
-     * deletes a specific patient from the database based on their ID.
-     * It first retrieves the existing patient by their ID. If the patient exists,
-     * it calls the deletePatient method from the PatientService class to delete the patient,
-     * and returns a status of OK.
-     * If the patient does not exist, it returns a status of NOT FOUND.
-     */
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id")int id){
-        Optional<Patient> patient = patientService.getPatient(id);
-        if(patient.isPresent()){
-            patientService.deletePatient(id);
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (patientService.deletePatient(id)) {
             return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/services/PatientService.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/services/PatientService.java
@@ -1,73 +1,52 @@
 package com.iwa.iwaid.consultoriomedico.services;
 
 import com.iwa.iwaid.consultoriomedico.entity.Patient;
+import com.iwa.iwaid.consultoriomedico.entity.PatientDTO;
+import com.iwa.iwaid.consultoriomedico.entity.PatientForm;
 import com.iwa.iwaid.consultoriomedico.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 
 @Service
 public class PatientService {
 
-    /** The Autowired annotation is used here to automatically inject the PatientRepository dependency.
-     * This means that Spring will find a bean of type PatientRepository and assign it to this field when
-     * the PatientService is created.
-     */
     @Autowired
     private PatientRepository patientRepository;
 
-    /** The getPatient() method retrieves a specific patient by their ID.
-     * It calls the findById method from PatientRepository, which returns an Optional
-     * that may contain the Patient if one with the given ID exists.
-     */
-    public Optional<Patient> getPatient(int id){
-        return patientRepository.findById(id);
-    }
-
-    /** The savePatient() method is responsible for saving a new patient to the database.
-     * It achieves this by calling the save method from the PatientRepository class.
-     * The savePatient() method then retrieves the ID of the saved Patient and returns it as the result.
-     */
-    public int savePatient(Patient patient){
-        Patient savePatient = patientRepository.save(patient);
-        return savePatient.getId();
-    }
-
-    /** The updatePatient() method updates an existing patient in the database.
-     * It first retrieves the existing patient by their ID. If the patient exists,
-     * it updates the patient's fields with the values from the Patient object passed as a parameter.
-     * It then calls the savePatient method to save the updated patient to the database.
-     * If the update is successful, it returns an Optional containing the updated patient.
-     * If the patient does not exist, it returns an empty Optional.
-     */
-    public Optional<Patient> updatePatient(Patient patient, int id){
-        Optional<Patient> patient1 = this.getPatient(id);
-        if(patient1.isPresent()){
-            Patient patientDB = patient1.orElseThrow();
-            patientDB.setName(patient.getName());
-            patientDB.setDateOfBirth(patient.getDateOfBirth());
-            patientDB.setGender(patient.getGender());
-            patientDB.setRfc(patient.getRfc());
-            patientDB.setAddress(patient.getAddress());
-            patientDB.setCity(patient.getCity());
-            patientDB.setPhoneNumber(patient.getPhoneNumber());
-            patientDB.setEmail(patient.getEmail());
-            this.savePatient(patientDB);
-            return Optional.of(patientDB);
+    public PatientDTO getPatient(final int id) {
+        Optional<Patient> patient = patientRepository.findById(id);
+        if (patient.isPresent()) {
+            return PatientDTO.build(patient.get());
+        } else {
+            return null;
         }
-        return Optional.empty();
     }
 
-    /** The deletePatient() method is responsible for deleting a specific patient from the database based on their ID.
-     * It accomplishes this by calling the deleteById method from the PatientRepository class.
-     * This deleteById() method specifically targets and removes the Patient record associated with the provided ID.
-     */
-    public void deletePatient(int id){
-        patientRepository.deleteById(id);
+    public PatientDTO savePatient(final PatientForm patientForm) {
+        Patient patient = new Patient(patientForm);
+        patientRepository.save(patient);
+        return PatientDTO.build(patient);
+    }
+
+    public PatientDTO updatePatient(final PatientForm patientForm, final int id) {
+        final Optional<Patient> patient = patientRepository.findById(id);
+        if (patient.isPresent()) {
+            patient.get().updatePatient(patientForm);
+            patientRepository.save(patient.get());
+            return PatientDTO.build(patient.get());
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deletePatient(int id) {
+        if (patientRepository.existsById(id)) {
+            patientRepository.deleteById(id);
+            return true;
+        } else
+            return false;
     }
 }
