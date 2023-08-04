c2b8df02ed0b70683f7e4da3babf81e22d6b50e5
DavidVelaz17 <139006505+DavidVelaz17@users.noreply.github.com>
2023-08-03 23:21:57 -0600

    Changes to repositories imports

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/repository/AppointmentRepository.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/repository/AppointmentRepository.java
@@ -1,11 +1,8 @@
 package com.iwa.iwaid.consultoriomedico.repository;
 
 import com.iwa.iwaid.consultoriomedico.entity.Appointment;
-import org.hibernate.annotations.Fetch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
