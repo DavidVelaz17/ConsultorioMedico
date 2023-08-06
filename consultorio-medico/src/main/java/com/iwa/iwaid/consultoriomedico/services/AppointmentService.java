88c23b6f1e052e963d481f2380629c7781bd4b25
DavidVelaz17 <139006505+DavidVelaz17@users.noreply.github.com>
2023-08-05 19:11:19 -0600

    Now you can't make an appointment with the same doctor, date and hour, it's the same with patient, also added some logs to validations

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/services/AppointmentService.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/services/AppointmentService.java
@@ -4,16 +4,20 @@ import com.iwa.iwaid.consultoriomedico.dto.AppointmentDTO;
 import com.iwa.iwaid.consultoriomedico.dto.DoctorDTO;
 import com.iwa.iwaid.consultoriomedico.dto.PatientDTO;
 import com.iwa.iwaid.consultoriomedico.entity.Appointment;
+import com.iwa.iwaid.consultoriomedico.entity.Hour;
 import com.iwa.iwaid.consultoriomedico.form.AppointmentForm;
 import com.iwa.iwaid.consultoriomedico.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AppointmentService {
 
     private final DoctorService doctorService;
@@ -47,6 +51,14 @@ public class AppointmentService {
 
     public AppointmentDTO createAppointment(final AppointmentForm form) throws Exception {
         final Appointment appointment = new Appointment(form);
+        validateDoctorAndDateAndHour(
+            appointment.getDoctorId(),
+            appointment.getDate(),
+            appointment.getHour());
+        validatePatientAndDateAndHour(
+            appointment.getPatientId(),
+            appointment.getDate(),
+            appointment.getHour());
         doctorService.validateIfDoctorExists(appointment.getDoctorId());
         patientService.validateIfPatientExist(appointment.getPatientId());
         appointmentRepository.save(appointment);
@@ -61,6 +73,14 @@ public class AppointmentService {
     public AppointmentDTO updateAppointmentById(final AppointmentForm form, final int id) throws Exception {
         validateIfAppointmentExists(id);
         final Appointment appointment = appointmentRepository.findById(id).get();
+        validateDoctorAndDateAndHour(
+            appointment.getDoctorId(),
+            appointment.getDate(),
+            appointment.getHour());
+        validatePatientAndDateAndHour(
+            appointment.getPatientId(),
+            appointment.getDate(),
+            appointment.getHour());
         doctorService.validateIfDoctorExists(appointment.getDoctorId());
         patientService.validateIfPatientExist(appointment.getPatientId());
         appointment.updateAppointment(form);
@@ -70,10 +90,31 @@ public class AppointmentService {
 
     private void validateIfAppointmentExists(int id) throws Exception {
         if (!appointmentRepository.existsById(id)) {
-            throw new Exception();
+            throw new Exception("The appointment does not exist yet");
         }
     }
 
+    private void validateDoctorAndDateAndHour(
+    final Integer doctorId,
+    final LocalDate date,
+    final Hour hour)
+        throws Exception {
+        if (appointmentRepository.existsByDoctorIdAndDateAndHour(doctorId,date, hour)) {
+            throw new Exception("This date and hour has been taken for this Doctor");
+        } else
+            log.info("The selected date and hour was available for this Doctor");
+    }
+
+    private void validatePatientAndDateAndHour(
+    final Integer patientId,
+    final LocalDate date,
+    final Hour hour)
+        throws Exception{
+        if(appointmentRepository.existsByPatientIdAndDateAndHour(patientId,date,hour)){
+            throw new Exception("This date and hour has been taken for this Patient");
+        }else
+            log.info("The selected date and hour was available for this Patient");
+    }
     private Map<Integer, DoctorDTO> getDoctorsMap(List<Integer> doctorsIds) {
         return doctorService.getDoctorsByIds(doctorsIds);
     }
