5a58be547602f0eda219c2255190a00a699734a7
davidvelaz17 <david.lara.velaz@gmail.com>
2023-08-22 20:46:18 -0600

    Better enums without useless imports

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/entity/Gender.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/entity/Gender.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
