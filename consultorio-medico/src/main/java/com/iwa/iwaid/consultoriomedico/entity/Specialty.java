55f5c803d932b48e4e66b38f9ff49103661a94dc
davidvelaz17 <david.lara.velaz@gmail.com>
2023-08-22 20:45:29 -0600

    Better enums

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/entity/Specialty.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/entity/Specialty.java
@@ -3,26 +3,19 @@ package com.iwa.iwaid.consultoriomedico.entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Getter
 @RequiredArgsConstructor
 public enum Specialty {
     General(0), Urologia(1), Ginecologia(2), Pediatria(3), Neurologia(4);
     private final int key;
     public static Specialty getSpecialty(Integer key) {
-        switch (key) {
-            case 0:
-                return Specialty.General;
-            case 1:
-                return Specialty.Urologia;
-            case 2:
-                return Specialty.Ginecologia;
-            case 3:
-                return Specialty.Pediatria;
-            case 4:
-                return Specialty.Neurologia;
-            default:
-                throw new IllegalArgumentException("Key [" + key
-                        + "] not supported.");
-        }
+        Map<Integer,Specialty> specialtyMap =
+          Arrays.stream(Specialty.values()).collect(Collectors.toMap(Specialty::getKey, Function.identity()));
+        return specialtyMap.get(key);
     }
 }
