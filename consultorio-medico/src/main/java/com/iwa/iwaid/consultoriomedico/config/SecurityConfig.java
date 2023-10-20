5e70435bd8246abab2d4e300303f993c345b711b
davidvelaz17 <david.lara.velaz@gmail.com>
2023-10-20 12:58:18 -0600

    NO CORS

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/config/SecurityConfig.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,7 +21,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authRequest -> authRequest.requestMatchers("/iwaid/auth/login")
                         .permitAll()
                         .anyRequest().authenticated())
