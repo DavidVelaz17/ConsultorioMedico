37ab04b91eb1c8f2fc27f6952e47af6951f1c703
davidvelaz17 <david.lara.velaz@gmail.com>
2023-10-20 14:02:04 -0600

    NO CORS and again

--- consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/config/SecurityConfig.java
+++ consultorio-medico/src/main/java/com/iwa/iwaid/consultoriomedico/config/SecurityConfig.java
@@ -19,12 +19,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
+
     @Bean
-    //@Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors().disable()
-                .csrf(csrf -> csrf.disable())
+                .cors()
+                .and()
+                .csrf().disable()
                 .authorizeHttpRequests(authRequest -> authRequest.requestMatchers("/iwaid/auth/login")
                         .permitAll()
                         .anyRequest().authenticated())
@@ -32,34 +33,5 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }/*
-    @Bean
-    @Order(2)
-    public SecurityFilterChain doctorsFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .cors(cors -> cors.disable())
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authRequest ->
-                        authRequest.requestMatchers
-                                ("/iwaid/doctors/**","/iwaid/patients/**","/iwaid/medicines/**","/iwaid/appointments/**",
-                                "/iwaid/medicalhistory/**","/iwaid/prescription/**").permitAll().anyRequest().hasRole("Doctor"))
-                .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
     }
-    @Bean
-    public SecurityFilterChain patientsFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .cors(cors -> cors.disable())
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authRequest ->
-                        authRequest.requestMatchers
-                                ("/iwaid/patients/**","/iwaid/appointments/**",
-                                        "/iwaid/medicalhistory/**","/iwaid/prescription/**").permitAll().anyRequest().hasRole("Patient"))
-                .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }*/
 }
